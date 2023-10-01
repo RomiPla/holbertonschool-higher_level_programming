68ddef9ba46c92aab592e947122df2265e8c8313
root <plaromina1997@gmail.com>
2023-10-01 14:03:29 -0700

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+	Defining an object class named Square
+"""
+
+
 class Square:
+    """
+        Creating the attribute for sai object
+    """
     def __init__(self, size):
         self.__size = size
-
