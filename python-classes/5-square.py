b0437fd0bc0af15bbcb6c96c7687f97ee5e7bc9b
root <plaromina1997@gmail.com>
2023-10-01 13:58:48 -0700

    5-square.py

---
+++ python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+	Defining an object class named Square
+"""
+
+
+class Square:
+    """
+        Creating the attribute for sai object
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Property setter for square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Defining size cualifications"""
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for y in range(self.__size):
+            print("#" * self.__size)
