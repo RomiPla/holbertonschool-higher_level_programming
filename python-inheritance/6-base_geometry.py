521289f15569a9b4649de150eb8a5c0449cd40ff
root <plaromina1997@gmail.com>
2023-10-07 23:16:02 -0700

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """Clase para geometría base"""
+
+    def area(self):
+        """Método para calcular el área"""
+        raise NotImplementedError("area() no está implementado")
+
