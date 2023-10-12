b5748a46d6729edea136d9159a74db38a9ee88f7
root <plaromina1997@gmail.com>
2023-10-12 16:54:02 -0700

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,154 +1,59 @@
 #!/usr/bin/python3
-"""
-ckd
-"""
 
 from models.base import Base
 
-
 class Rectangle(Base):
-    """ class Rectangle from Base """
-
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ init Rectangle class """
-
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
-    @property
-    def width(self):
-        """Ro"""
+        # Getter and setter for width
+        @property
+        def width(self):
+            return self.__width
 
-        return self.__width
+        @width.setter
+        def width(self, value):
+            self.__width = value
 
-    @width.setter
-    def width(self, width):
-        """Ro"""
+        # Getter and setter for height
+        @property
+        def height(self):
+            return self.__height
 
-        self.__width = width
+        @height.setter
+        def height(self, value):
+            self.__height = value
 
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        elif width <= 0:
-            raise ValueError("width must be >= 0")
+        # Getter and setter for x
+        @property
+        def x(self):
+            return self.__x
 
-    @property
-    def height(self):
-        """Ro"""
+        @x.setter
+        def x(self, value):
+            self.__x = value
 
-        return self.__height
+        # Getter and setter for y
+        @property
+        def y(self):
+            return self.__y
 
-    @height.setter
-    def height(self, height):
-        """Ro"""
+        @y.setter
+        def y(self, value):
+            self.__y = value
 
-        self.__height = height
+# Test your implementation
+if __name__ == "__main__":
+    r1 = Rectangle(10, 2)
+    print(r1.id)
 
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        elif height <= 0:
-            raise ValueError("height must be >= 0")
+    r2 = Rectangle(2, 10)
+    print(r2.id)
 
-    @property
-    def x(self):
-        """Ro"""
+    r3 = Rectangle(10, 2, 0, 0, 12)
+    print(r3.id)
 
-        return self.__x
-
-    @x.setter
-    def x(self, x):
-        """Ro"""
-
-        self.__x = x
-        if type(x) is not int:
-            raise TypeError("x must be an integer")
-        elif x < 0:
-            raise ValueError("x must be > 0")
-
-    @property
-    def y(self):
-        """Ro"""
-
-        return self.__y
-
-    @y.setter
-    def y(self, y):
-        """Ro"""
-
-        self.__y = y
-        if type(y) is not int:
-            raise TypeError("y must be an integer")
-        elif y < 0:
-            raise ValueError("y must be > 0")
-
-    def area(self):
-        """Ro"""
-
-        return self.width * self.height
-
-    def display(self):
-        """Ro"""
-
-        for i in range(self.__y):
-            print()
-        for i in range(self.__height):
-            for j in range(self.__x):
-                print(" ", end="")
-            for j in range(self.__width):
-                print("#", end="")
-            print()
-
-    def __str__(self):
-        """Ro"""
-
-        return("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
-               self.__y, self.__width, self.__height))
-
-    def update(self, *args, **kwargs):
-        """Ro"""
-
-        if len(args) > 0:
-
-            for i in range(len(args)):
-
-                if i == 0:
-                    self.id = args[0]
-                if i == 1:
-                    self.width = args[1]
-                if i == 2:
-                    self.height = args[2]
-                if i == 3:
-                    self.x = args[3]
-                if i == 4:
-                    self.y = args[4]
-        else:
-            if len(kwargs) > 0:
-
-                k = kwargs.keys()
-
-                for i in k:
-
-                    if i == 'id':
-                        self.id = kwargs['id']
-                    if i == 'width':
-                        self.width = kwargs['width']
-                    if i == 'height':
-                        self.height = kwargs['height']
-                    if i == 'x':
-                        self.x = kwargs['x']
-                    if i == 'y':
-                        self.y = kwargs['y']
-
-    def to_dictionary(self):
-        """Ro"""
-
-        dictionary = {}
-        dictionary['id'] = self.id
-        dictionary['width'] = self.width
-        dictionary['height'] = self.height
-        dictionary['x'] = self.x
-        dictionary['y'] = self.y
-        return dictionary
