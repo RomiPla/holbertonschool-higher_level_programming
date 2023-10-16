42957f1d1c245fb5d674a9bffe62662dfddece02
root <plaromina1997@gmail.com>
2023-10-15 18:37:31 -0700

    square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,79 +1,65 @@
 #!/usr/bin/python3
 """
-Creation of the class Square that inherits from Rectangle
+	Module
 """
-
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ class Square inherits from Rectangle """
+    """Class Denominator"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ Constructor of Square """
-
+        """__init__"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """Ro"""
-
-        return("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
-                                                 self.width))
+        """String representation"""
+        return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
 
     @property
     def size(self):
-        """Ro"""
-
+        """Property width"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """Ro"""
-
-        self.width = value
-        self.height = value
+        """Defining size cualifications"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 1:
+            raise ValueError("width must be > 0")
+        self.__size = value
+        self.width = self.__size
 
     def update(self, *args, **kwargs):
-        """Ro"""
-
-        if len(args) > 0:
-
-            for i in range(len(args)):
-
-                if i == 0:
-                    self.id = args[0]
-                if i == 1:
-                    self.width = args[1]
-                    self.height = args[1]
-                if i == 2:
-                    self.x = args[2]
-                if i == 3:
-                    self.y = args[3]
-
-        else:
-
-            if len(kwargs) > 0:
-
-                k = kwargs.keys()
-
-                for i in k:
-
-                    if i == 'id':
-                        self.id = kwargs['id']
-                    if i == 'size':
-                        self.width = kwargs['size']
-                        self.height = kwargs['size']
-                    if i == 'x':
-                        self.x = kwargs['x']
-                    if i == 'y':
-                        self.y = kwargs['y']
+        """Updates the values"""
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        if kwargs:
+            k = kwargs.keys()
+            for i in k:
+                if i == 'id':
+                    self.id = kwargs['id']
+                if i == 'size':
+                    self.width = kwargs['size']
+                if i == 'x':
+                    self.x = kwargs['x']
+                if i == 'y':
+                    self.y = kwargs['y']
 
     def to_dictionary(self):
-        """Ro"""
+        """Creates a dictionary and returns it"""
 
         dictionary = {}
         dictionary['id'] = self.id
-        dictionary['size'] = self.width
         dictionary['x'] = self.x
+        dictionary['size'] = self.width
         dictionary['y'] = self.y
         return dictionary
