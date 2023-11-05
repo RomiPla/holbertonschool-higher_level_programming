49eba8964b128962be3de253f6a7804e9eb4bd9e
RomiPla <135638208+RomiPla@users.noreply.github.com>
2023-11-05 16:30:29 -0300

    update

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Module to save obj inside filename
+"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+        Function
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(json.dumps(my_obj))
