1c8d809bb06f251c972a84bc176f4d71e2d3a6b7
RomiPla <135638208+RomiPla@users.noreply.github.com>
2023-11-05 16:28:36 -0300

    update

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Task 3
+"""
+import json
+"""
+    Import module to use json
+"""
+
+
+def to_json_string(my_obj):
+    """
+        Function
+    """
+    return json.dumps(my_obj)
