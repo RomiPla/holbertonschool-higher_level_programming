6321a92db6325031c1d2b10c371247fad6a49fe3
RomiPla <135638208+RomiPla@users.noreply.github.com>
2023-11-05 16:29:29 -0300

    update

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Task 3
+"""
+import json
+"""
+    Import module to use json
+"""
+
+
+def from_json_string(my_str):
+    """
+        Function
+    """
+    return json.loads(my_str)
