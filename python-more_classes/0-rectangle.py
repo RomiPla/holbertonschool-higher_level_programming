4d8808ef4f6c68d3fd052c7c6b651de203d08a7a
RomiPla <plaromina1997@gmail.com>
2024-02-08 21:50:27 -0300

    update

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    Se define una clase vacia llamada rectangle
+"""
+
+class rectangle:
+    """
+        Requisitos para hacer una clase vacia de tipo rectangle
+    """
+
+    pass
