98d3820363d5a96956b2a22f833dad667726ad7a
RomiPla <135638208+RomiPla@users.noreply.github.com>
2024-05-09 10:00:02 -0300

    Delete python-if_else_loops_functions directory

--- python-if_else_loops_functions/2-print_alphabet.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-ch = 97
-while ch <= 122:
-    print("{}" .format(chr(ch)), end='')
-    ch += 1
--- python-if_else_loops_functions/7-islower.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def islower(c):
-    x = ord(c)
-    if x > 96 and x < 124:
-        return (True)
-    else:
-        return (False)
