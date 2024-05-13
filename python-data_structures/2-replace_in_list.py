ea2f3563484da5822ff038ffd97cc190ddb46b4a
RomiPla <plaromina1997@gmail.com>
2024-05-13 10:16:12 -0300

    update

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list)-1:
-        return(idx+1)
+    if (0 <= idx < len (my_list)):
+        my:list [idx] = element
+        return (my_list)
