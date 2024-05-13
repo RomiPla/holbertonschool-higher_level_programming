f637b0c55997764c32e2792a200ebfed8c31e95a
RomiPla <plaromina1997@gmail.com>
2024-05-13 11:37:16 -0300

    update

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    if my_list is not None:
-        my_list.copy()
-        for a in my_list:
-            print("{:d}".format(a))
+    if (idx < 0) or (idx >= le(my_list)):
+        return my_list
+    else:
+        new_list = my_list[:]
+        new_list [idx] = element
+        return new_list
