1f835d59b6dc6251f0af5b329035a5299aaded5f
RomiPla <plaromina1997@gmail.com>
2024-05-19 23:11:34 -0300

    correccion

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in range(x):
-            print("{}".format(my_list[i]), end='')
+        for y in range(x):
+            print("{}".format(my_list[y]), end='')
     except IndexError:
-        return i
+        return y
     else:
         return x
     finally:
         print("")
-    
