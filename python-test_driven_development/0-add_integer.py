4ab0729e7a5e72d74ca71aaab6b2f99ec03a806b
RomiPla <plaromina1997@gmail.com>
2024-05-22 11:58:33 -0300

    modulo

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,7 +2,12 @@
 """
     dshfhfhdfgjfdgjkfgg
 """
+
 def add_integer(a, b=98):
+    """
+        ihggehiugerhui
+    """
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
