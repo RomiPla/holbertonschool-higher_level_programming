3cba03711916e070e9bdc58fd40f9efd878afb7a
RomiPla <plaromina1997@gmail.com>
2024-05-23 08:54:29 -0300

    correccion 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """
-    This function splits a given text by ". , ? :"
-    It takes those characters and puts a new line after them
+    fdhkufhfkfhkfjdv
 """
 
 
 def text_indentation(text):
     """
-        As said previously, it splits the given text.
-        But first it checks if it's a string.
-        It gives an error message if it isn't a string
+       ndsjkdnbvjsd
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
