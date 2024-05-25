5a2470221d55086b0396680302069478fbdacd80
RomiPla <plaromina1997@gmail.com>
2024-05-25 19:29:49 -0300

    task 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+    Defining an object class named Square
+"""
+
+
+class Rectangle:
+    """
+        Creating the attribute for sai object
+    """
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        """Property setter for square"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Defining height cualifications"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """Property setter for square"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Defining width cualifications"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        """Property setter for square"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Defining perimeter cualifications"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return self.__height * 2 + self.__width * 2
