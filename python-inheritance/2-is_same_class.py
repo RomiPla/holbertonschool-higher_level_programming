82275d7405de62ce29227f85560a41cc7e18a5ee
RomiPla <plaromina1997@gmail.com>
2024-05-25 22:26:56 -0300

    task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Módulo para verificar si obj es 
+    de la clase pasada
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+        Verdadero si el objeto es 
+        exactamente una instancia de la 
+        clase especificada.
+        De lo contrario, Falso.
+
+    """
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
