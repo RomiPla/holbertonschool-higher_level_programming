74f4d86600e3e866bb9cdc7a2873aaaac9419e61
RomiPla <plaromina1997@gmail.com>
2024-05-26 22:52:07 -0300

    task 3

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+    fwbekjjfbekwj
+"""
+
+class CountedIterator:
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.counter = 0
+
+    def __next__(self):
+        try:
+            item = next(self.iterator)
+            self.counter += 1
+            return item
+        except StopIteration:
+            raise StopIteration
+
+    def get_count(self):
+        return self.counter
+
+    def __iter__(self):
+        return self
