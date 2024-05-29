f676ab92a59c3b7caa9f1726a0759a0ae1fd59fd
RomiPla <plaromina1997@gmail.com>
2024-05-29 11:37:43 -0300

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""
-    fdghdfkgjfgd
-"""
 def read_file(filename=""):
     """
     Reads a text file (UTF8) and prints it to stdout.
-    Args:
-    filename (str): The name of the file to read. Default is an empty string.
-    """
 
+    Args:
+        filename (str): The name of the file to read. Default is an empty string.
+    """
     with open(filename, "r", encoding="utf-8") as file:
-        print(file.read())
+        content = file.read()
+        print(content, end="")
