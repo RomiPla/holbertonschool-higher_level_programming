371c54ebc6024a6c485edf40bb63007522d9daca
RomiPla <plaromina1997@gmail.com>
2024-05-31 23:21:07 -0300

    task 4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Task 4
+"""
+import json
+"""
+    Importar modulo para usar json
+"""
+
+
+def from_json_string(my_str):
+    """
+        Funcion
+    """
+    return json.loads(my_str)
