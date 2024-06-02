b61422633606d4ee0c15a2aa89d7fe957b29e4ab
RomiPla <plaromina1997@gmail.com>
2024-06-02 11:39:51 -0300

    update

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -14,7 +14,9 @@ class Student:
         """
             Init
         """
-        
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self, attrs=None):
         """
