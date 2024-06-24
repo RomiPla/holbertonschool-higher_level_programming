c7f75b88964cbac6de29164f9f8bf750e543c01a
RomiPla <plaromina1997@gmail.com>
2024-06-23 22:08:37 -0300

    task 6

--- SQL_more_queries/16-shows_by_genre.sql
+++
@@ -1,4 +0,0 @@
--- Script para crear una tabla dentro de una base de datos
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
