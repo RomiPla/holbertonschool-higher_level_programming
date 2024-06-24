a5c3f4dc6e2b8029cc02fc236623c79e61698d75
RomiPla <plaromina1997@gmail.com>
2024-06-23 22:54:55 -0300

    task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    Lists all the states in the database
+    that start with the letter N hbtn_0e_0_usa
+"""
+
+if __name__ == "__main__":
+    """
+        Connects to the database and selects the list of states in the database
+    """
+    import MySQLdb
+    import sys
+
+    data_connector = MySQLdb.connect(host="localhost",
+                                     port=3306,
+                                     user=sys.argv[1],
+                                     passwd=sys.argv[2],
+                                     db=sys.argv[3])
+    cursor = data_connector.cursor()
+    cursor.execute(f"SELECT * FROM states WHERE name LIKE BINARY \
+        '{sys.argv[4]}' ORDER BY id;")
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    data_connector.close()
